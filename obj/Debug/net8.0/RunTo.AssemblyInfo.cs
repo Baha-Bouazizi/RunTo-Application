bd5e1c29b652987bcf26bf1b323556df874149c1
Bouazizi Baha <166825233+Baha-Bouazizi@users.noreply.github.com>
2024-06-28 18:07:41 +0100

    changement des coleurs

--- obj/Debug/net8.0/RunTo.AssemblyInfo.cs
+++ obj/Debug/net8.0/RunTo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunTo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7baec44f5b88494add6950410e76f46d89e41d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1e8a53d7da9e890a0dcbdb267d72c49731497d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunTo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunTo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
